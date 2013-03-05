fdcfda1c14ba892afc4f75f9b732b9561b0156e3
Sommer <sommerawesome@gmail.com>
2013-03-05 13:10:01 +0100

    Renamed Webservice to "Stalkr_WebService"

---
+++ Stalkr_Service/src/java/easj/jrpg/Stalkr_WebService.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package easj.jrpg;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+
+/**
+ *
+ * @author Else Larsen
+ */
+@WebService(serviceName = "Stalkr_WebService")
+public class Stalkr_WebService {
+
+    /**
+     * This is a sample web service operation
+     */
+    @WebMethod(operationName = "hello")
+    public String hello(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+}
