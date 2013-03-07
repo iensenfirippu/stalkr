5f650f0d779673f6959bceef64dcac24a808d828
Philip Jensen <me@iensenfirippu.dk>
2013-03-07 12:00:04 +0100

    database commit test
    Scripts are not final or working, this is a subfolder upload test...

---
+++ database/stalkr.sql
@@ -0,0 +1,67 @@
+﻿--DROP DATABASE IF EXISTS stalkr;
+--SELECT DATABASE stalkr;
+
+CREATE TABLE 'user' (
+  'id' int(11) NOT NULL auto_increment,
+  'username' varchar(50) NOT NULL,
+  'password' varchar(255) NOT NULL,
+  'location_latitude' float(11) NOT NULL,
+  'location_longitude' float(11) NOT NULL,
+  'location_timestamp' int(11) NOT NULL,
+  'info_description' int(11) NOT NULL,
+  'pref_description' int(11) NOT NULL,
+  PRIMARY KEY ('id')
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8_bin AUTO_INCREMENT=3;
+
+-- CREATE TABLE 'preference' (
+--   'user' int(11) NOT NULL auto_increment,
+--   'description' int(11) NOT NULL,
+--   PRIMARY KEY ('id')
+-- ) ENGINE=InnoDB  DEFAULT CHARSET=utf8_bin AUTO_INCREMENT=3;
+
+CREATE TABLE 'desciption' (
+  'id' int(11) NOT NULL auto_increment,
+  'timestamp' int(11) NOT NULL,
+
+  'firstname' varchar(50) NOT NULL,
+  'lastname' varchar(50) NOT NULL,
+  'age' int(11) NOT NULL,
+  'age_max' int(11) NOT NULL,
+  'gender' varchar(255) NOT NULL,
+  'birthday' int(11) NOT NULL,
+  'smoking' varchar(255) NOT NULL,
+  'drinking' varchar(255) NOT NULL,
+  'orientation' varchar(255) NOT NULL,
+  'ethnicity' varchar(255) NOT NULL,
+  'nationality' varchar(255) NOT NULL,
+  'religion' varchar(255) NOT NULL,
+  'zodiac' varchar(255) NOT NULL,
+  'chi_zodiac' varchar(255) NOT NULL,
+  'bloodtype' varchar(255) NOT NULL,
+
+  'country' int(11) NOT NULL,
+  'region' int(11) NOT NULL,
+  'city' int(11) NOT NULL,
+
+  'bodytype' varchar(255) NOT NULL,
+  'height' int(11) NOT NULL,
+  'height_max' int(11) NOT NULL,
+  'weight' int(11) NOT NULL,
+  'weight_max' int(11) NOT NULL,
+  'haircolor' varchar(255) NOT NULL,
+  'eyecolor' varchar(255) NOT NULL,
+  'skincolor' varchar(255) NOT NULL,
+  'tattoo' varchar(255) NOT NULL,
+  'piercing' varchar(255) NOT NULL,
+
+  PRIMARY KEY  ('id')
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8_bin AUTO_INCREMENT=3;
+
+CREATE TABLE 'report' (
+  'id' int(11) NOT NULL auto_increment,
+  'timestamp' int(11) NOT NULL,
+  'reporter' int(11) NOT NULL,
+  'reportee' int(11) NOT NULL,
+  'description' blob NOT NULL,
+  PRIMARY KEY  ('id')
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8_bin AUTO_INCREMENT=3;
---
+++ database/stalkr_v001.sql
@@ -0,0 +1,58 @@
+﻿CREATE TABLE `user` (
+  `id` INT(11) NOT NULL auto_increment,
+  `username` VARCHAR(50) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
+  `location_latitude` FLOAT(11) NOT NULL,
+  `location_longitude` FLOAT(11) NOT NULL,
+  `location_timestamp` INT(11) NOT NULL,
+  `info_description` INT(11) NOT NULL,
+  `pref_description` INT(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=`InnoDB` DEFAULT CHARSET=`utf8`;
+
+CREATE TABLE `desciption` (
+  `id` INT(11) NOT NULL auto_increment,
+  `timestamp INT(11) NOT NULL,
+
+  `firstname` VARCHAR(50) NOT NULL,
+  `lastname` VARCHAR(50) NOT NULL,
+  `age` INT(11) NOT NULL,
+  `age_max` INT(11) NOT NULL,
+  `gender` VARCHAR(255) NOT NULL,
+  `birthday` INT(11) NOT NULL,
+  `smoking` VARCHAR(255) NOT NULL,
+  `drinking` VARCHAR(255) NOT NULL,
+  `orientation` VARCHAR(255) NOT NULL,
+  `ethnicity` VARCHAR(255) NOT NULL,
+  `nationality` VARCHAR(255) NOT NULL,
+  `religion` VARCHAR(255) NOT NULL,
+  `zodiac` VARCHAR(255) NOT NULL,
+  `chi_zodiac` VARCHAR(255) NOT NULL,
+  `bloodtype` VARCHAR(255) NOT NULL,
+
+  `country` INT(11) NOT NULL,
+  `region` INT(11) NOT NULL,
+  `city` INT(11) NOT NULL,
+
+  `bodytype` VARCHAR(255) NOT NULL,
+  `height` INT(11) NOT NULL,
+  `height_max` INT(11) NOT NULL,
+  `weight` INT(11) NOT NULL,
+  `weight_max` INT(11) NOT NULL,
+  `haircolor` VARCHAR(255) NOT NULL,
+  `eyecolor` VARCHAR(255) NOT NULL,
+  `skincolor` VARCHAR(255) NOT NULL,
+  `tattoo` VARCHAR(255) NOT NULL,
+  `piercing` VARCHAR(255) NOT NULL,
+
+  PRIMARY KEY (`id`)
+) ENGINE=`InnoDB` DEFAULT CHARSET=`utf8`;
+
+CREATE TABLE report (
+  `id` INT(11) NOT NULL auto_increment,
+  `timestamp` INT(11) NOT NULL,
+  `reporter` INT(11) NOT NULL,
+  `reportee` INT(11) NOT NULL,
+  `description` BLOB NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=`InnoDB` DEFAULT CHARSET=`utf8`;
