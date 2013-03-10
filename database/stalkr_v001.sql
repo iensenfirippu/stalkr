1ce8e6ce569447fc62940dff2a1ca33ade4b6c4c
Philip Jensen <me@iensenfirippu.dk>
2013-03-11 00:04:25 +0100

    Database update
    Updated database script with less fields... (to make Sommer happy)

--- database/stalkr_v001.sql
+++ database/stalkr_v001.sql
@@ -1,58 +1,105 @@
-﻿CREATE TABLE `user` (
-  `id` INT(11) NOT NULL auto_increment,
-  `username` VARCHAR(50) NOT NULL,
-  `password` VARCHAR(255) NOT NULL,
-  `location_latitude` FLOAT(11) NOT NULL,
-  `location_longitude` FLOAT(11) NOT NULL,
-  `location_timestamp` INT(11) NOT NULL,
-  `info_description` INT(11) NOT NULL,
-  `pref_description` INT(11) NOT NULL,
-  PRIMARY KEY (`id`)
+﻿DROP TABLE IF EXISTS `user`;
+DROP TABLE IF EXISTS `description`;
+DROP TABLE IF EXISTS `report`;
+
+CREATE TABLE IF NOT EXISTS `user` (
+	`id` INT(11) NOT NULL auto_increment,
+	`username` VARCHAR(50) NOT NULL,
+	`password` VARCHAR(255) NOT NULL,
+	`firstname` VARCHAR(50) NULL,
+	`lastname` VARCHAR(50) NULL,
+	`location_latitude` FLOAT(11) NOT NULL,
+	`location_longitude` FLOAT(11) NOT NULL,
+	`location_timestamp` INT(11) NOT NULL,
+	`info_description` INT(11) NOT NULL,
+	`pref_description` INT(11) NOT NULL,
+	PRIMARY KEY (`id`)
 ) ENGINE=`InnoDB` DEFAULT CHARSET=`utf8`;
 
-CREATE TABLE `desciption` (
-  `id` INT(11) NOT NULL auto_increment,
-  `timestamp INT(11) NOT NULL,
-
-  `firstname` VARCHAR(50) NOT NULL,
-  `lastname` VARCHAR(50) NOT NULL,
-  `age` INT(11) NOT NULL,
-  `age_max` INT(11) NOT NULL,
-  `gender` VARCHAR(255) NOT NULL,
-  `birthday` INT(11) NOT NULL,
-  `smoking` VARCHAR(255) NOT NULL,
-  `drinking` VARCHAR(255) NOT NULL,
-  `orientation` VARCHAR(255) NOT NULL,
-  `ethnicity` VARCHAR(255) NOT NULL,
-  `nationality` VARCHAR(255) NOT NULL,
-  `religion` VARCHAR(255) NOT NULL,
-  `zodiac` VARCHAR(255) NOT NULL,
-  `chi_zodiac` VARCHAR(255) NOT NULL,
-  `bloodtype` VARCHAR(255) NOT NULL,
-
-  `country` INT(11) NOT NULL,
-  `region` INT(11) NOT NULL,
-  `city` INT(11) NOT NULL,
-
-  `bodytype` VARCHAR(255) NOT NULL,
-  `height` INT(11) NOT NULL,
-  `height_max` INT(11) NOT NULL,
-  `weight` INT(11) NOT NULL,
-  `weight_max` INT(11) NOT NULL,
-  `haircolor` VARCHAR(255) NOT NULL,
-  `eyecolor` VARCHAR(255) NOT NULL,
-  `skincolor` VARCHAR(255) NOT NULL,
-  `tattoo` VARCHAR(255) NOT NULL,
-  `piercing` VARCHAR(255) NOT NULL,
-
-  PRIMARY KEY (`id`)
+CREATE TABLE IF NOT EXISTS `description` (
+	`id` INT(11) NOT NULL auto_increment,
+	`timestamp` INT(11) NOT NULL,
+	`age` INT(11) NULL,
+	`age_max` INT(11) NULL,
+	`gender` VARCHAR(255) NULL,
+	`birthday` INT(11) NULL,
+	`smoking` VARCHAR(255) NULL,
+	`drinking` VARCHAR(255) NULL,
+	`orientation` VARCHAR(255) NULL,
+	`ethnicity` VARCHAR(255) NULL,
+	`nationality` VARCHAR(255) NULL,
+	`religion` VARCHAR(255) NULL,
+	`zodiac` VARCHAR(255) NULL,
+	`chi_zodiac` VARCHAR(255) NULL,
+	`bloodtype` VARCHAR(255) NULL,
+	`country` VARCHAR(255) NULL,
+	`region` VARCHAR(255) NULL,
+	`city` VARCHAR(255) NULL,
+	`bodytype` VARCHAR(255) NULL,
+	`height` INT(11) NULL,
+	`height_max` INT(11) NULL,
+	`weight` INT(11) NULL,
+	`weight_max` INT(11) NULL,
+	`haircolor` VARCHAR(255) NULL,
+	`eyecolor` VARCHAR(255) NULL,
+	`skincolor` VARCHAR(255) NULL,
+	`tattoo` VARCHAR(255) NULL,
+	`piercing` VARCHAR(255) NULL,
+	PRIMARY KEY (`id`)
 ) ENGINE=`InnoDB` DEFAULT CHARSET=`utf8`;
 
-CREATE TABLE report (
-  `id` INT(11) NOT NULL auto_increment,
-  `timestamp` INT(11) NOT NULL,
-  `reporter` INT(11) NOT NULL,
-  `reportee` INT(11) NOT NULL,
-  `description` BLOB NOT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE IF NOT EXISTS `report` (
+	`id` INT(11) NOT NULL auto_increment,
+	`timestamp` INT(11) NOT NULL,
+	`reporter` INT(11) NOT NULL,
+	`reportee` INT(11) NOT NULL,
+	`description` BLOB NOT NULL,
+	PRIMARY KEY (`id`)
 ) ENGINE=`InnoDB` DEFAULT CHARSET=`utf8`;
+
+INSERT INTO `user` (
+	`id`,`username`,`password`,`firstname`,`lastname`,`location_latitude`,
+	`location_longitude`,`location_timestamp`,
+	`info_description`,`pref_description`
+) VALUES (
+	NULL,'JohnDoe69','1234','John','Doe','10.5','5.10',
+	'123456789', '1', '2'
+);
+
+INSERT INTO `description` (
+	`id`,`timestamp`,`age`,
+	`age_max`,`gender`,`birthday`,`smoking`,
+	`drinking`,`orientation`,`ethnicity`,
+	`nationality`,`religion`,`zodiac`,`chi_zodiac`,
+	`bloodtype`,`country`,`region`,`city`,`bodytype`,
+	`height`,`height_max`,`weight`,`weight_max`,
+	`haircolor`,`eyecolor`,`skincolor`,`tattoo`,
+	`piercing`
+) VALUES (
+	NULL,'123456789','25','0','1',
+	'000000123','1','1','1','1','1','1','1','1','1',
+	'1','1','1','1','180','0','70','0','1','1','1',
+	'1', '1'
+);
+
+INSERT INTO `description` (
+	`id`,`timestamp`,`age`,
+	`age_max`,`gender`,`birthday`,`smoking`,
+	`drinking`,`orientation`,`ethnicity`,
+	`nationality`,`religion`,`zodiac`,`chi_zodiac`,
+	`bloodtype`,`country`,`region`,`city`,`bodytype`,
+	`height`,`height_max`,`weight`,`weight_max`,
+	`haircolor`,`eyecolor`,`skincolor`,`tattoo`,
+	`piercing`
+) VALUES (
+	NULL,'123456789','20','30','2',
+	NULL,'1','1','1','1','1','1','1','1','1',
+	'1','1','1','1','160','200','50','80','1','1','1',
+	'1', '1'
+);
+
+INSERT INTO `report` (
+	`id`,`timestamp`,`reporter`,`reportee`,`description`
+) VALUES (
+	NULL,'123456789','1','1','Blah blah blah'
+);
---
+++ database/stalkr_v002.sql
@@ -0,0 +1,97 @@
+﻿DROP TABLE IF EXISTS `user`;
+DROP TABLE IF EXISTS `description`;
+DROP TABLE IF EXISTS `report`;
+
+CREATE TABLE IF NOT EXISTS `user` (
+	`id` INT(11) NOT NULL auto_increment,
+	`username` VARCHAR(50) NOT NULL,
+	`password` VARCHAR(255) NOT NULL,
+	`firstname` VARCHAR(50) NULL,
+	`lastname` VARCHAR(50) NULL,
+	`birthday` INT(11) NULL,
+	`location_latitude` FLOAT(11) NOT NULL,
+	`location_longitude` FLOAT(11) NOT NULL,
+	`location_timestamp` INT(11) NOT NULL,
+	`info_description` INT(11) NOT NULL,
+	`pref_description` INT(11) NOT NULL,
+	PRIMARY KEY (`id`)
+) ENGINE=`InnoDB` DEFAULT CHARSET=`utf8`;
+
+CREATE TABLE IF NOT EXISTS `description` (
+	`id` INT(11) NOT NULL auto_increment,
+	`timestamp` INT(11) NOT NULL,
+	`country` VARCHAR(255) NULL,
+	`region` VARCHAR(255) NULL,
+	`city` VARCHAR(255) NULL,
+	`age` INT(11) NULL,
+	`age_max` INT(11) NULL,
+	`gender` VARCHAR(255) NULL,
+	PRIMARY KEY (`id`)
+) ENGINE=`InnoDB` DEFAULT CHARSET=`utf8`;
+#	`smoking` VARCHAR(255) NULL,
+#	`drinking` VARCHAR(255) NULL,
+#	`orientation` VARCHAR(255) NULL,
+#	`ethnicity` VARCHAR(255) NULL,
+#	`nationality` VARCHAR(255) NULL,
+#	`religion` VARCHAR(255) NULL,
+#	`zodiac` VARCHAR(255) NULL,
+#	`chi_zodiac` VARCHAR(255) NULL,
+#	`bloodtype` VARCHAR(255) NULL,
+#	`bodytype` VARCHAR(255) NULL,
+#	`height` INT(11) NULL,
+#	`height_max` INT(11) NULL,
+#	`weight` INT(11) NULL,
+#	`weight_max` INT(11) NULL,
+#	`haircolor` VARCHAR(255) NULL,
+#	`eyecolor` VARCHAR(255) NULL,
+#	`skincolor` VARCHAR(255) NULL,
+#	`tattoo` VARCHAR(255) NULL,
+#	`piercing` VARCHAR(255) NULL,
+
+CREATE TABLE IF NOT EXISTS `report` (
+	`id` INT(11) NOT NULL auto_increment,
+	`timestamp` INT(11) NOT NULL,
+	`reporter` INT(11) NOT NULL,
+	`reportee` INT(11) NOT NULL,
+	`description` BLOB NOT NULL,
+	PRIMARY KEY (`id`)
+) ENGINE=`InnoDB` DEFAULT CHARSET=`utf8`;
+
+INSERT INTO `user` (
+	`id`,`username`,`password`,`firstname`,`lastname`,`birthday`,`location_latitude`,
+	`location_longitude`,`location_timestamp`,
+	`info_description`,`pref_description`
+) VALUES (
+	NULL,'JohnDoe69','1234','John','Doe','000000123','10.5','5.10',
+	'123456789', '1', '2'
+);
+
+INSERT INTO `description` (
+	`id`,`timestamp`,`country`,`region`,`city`,`age`,`age_max`,`gender`
+) VALUES (
+	NULL,'123456789','1','1','1','25','0','1'
+);
+#	,`smoking`,`drinking`,`orientation`,`ethnicity,`nationality`
+#	,`religion`,`zodiac`,`chi_zodiac`,`bloodtype`,`bodytype`,`height`,`height_max`
+#	,`weight`,`weight_max`,`haircolor`,`eyecolor`,`skincolor`,`tattoo`,`piercing`
+#	,'1','1','1','1','1'
+#	,'1','1','1','1','1','180','0'
+#	,'70','0','1','1','1','1', '1'
+
+INSERT INTO `description` (
+	`id`,`timestamp`,`country`,`region`,`city`,`age`,`age_max`,`gender`
+) VALUES (
+	NULL,'123456789','1','1','1','25','0','1'
+);
+#	,`smoking`,`drinking`,`orientation`,`ethnicity,`nationality`
+#	,`religion`,`zodiac`,`chi_zodiac`,`bloodtype`,`bodytype`,`height`,`height_max`
+#	,`weight`,`weight_max`,`haircolor`,`eyecolor`,`skincolor`,`tattoo`,`piercing`
+#	,'1','1','1','1','1'
+#	,'1','1','1','1','1','160','200'
+#	,'50','80','1','1','1','1', '1'
+
+INSERT INTO `report` (
+	`id`,`timestamp`,`reporter`,`reportee`,`description`
+) VALUES (
+	NULL,'123456789','1','1','Blah blah blah'
+);
