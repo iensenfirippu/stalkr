4e19c2e24ea8a4bc55be5233f1a007fe9809b077
Philip Jensen <me@iensenfirippu.dk>
2013-03-28 17:23:24 +0100

    Javafied Library plus
    Added a method that returns a list of five test users.
    Toolbelt.getTestUsers()

--- Netbeans/javalibtest/src/javalibtest/Javalibtest.java
+++ Netbeans/javalibtest/src/javalibtest/Javalibtest.java
@@ -20,48 +20,11 @@ public class Javalibtest
 	 */
 	public static void main(String[] args)
 	{
-		User u = new User();
-		u.setEmail("me@iensenfirippu.dk");
-		u.setUsername("iensensama");
-		u.setPassword("Always1234");
-		u.setFirstName("Philip");
-		u.setLastName("Jensen");
-		u.setBirthday(new Date());
+		ArrayList<User> users = Toolbelt.getTestUsers();
 		
-		GeoLocation l = new GeoLocation(2.2F, 3.3F);
-		u.setLocation(l);
-		
-		Description d = new Description();
-		d.setTimeStamp(new Date());
-		d.setTitle("Default");
-		d.setAge(new Range(26));
-		d.setGender(new GenderList(GenderType.MALE));
-		d.setSexuality(new SexualityList(SexualityType.HETERO));
-		d.setArea(new AreaList(AreaType.SJAELLAND));
-		d.setSmoking(new SmokingList(SmokingType.NO));
-		d.setDrinking(new DrinkingList(DrinkingType.YES));
-		u.setDescription(d);
-		
-		Description p = new Description();
-		p.setTimeStamp(new Date());
-		p.setTitle("Default");
-		p.setAge(new Range(25, 40));
-		p.setGender(GenderList.FromString("11111"));
-		p.setSexuality(SexualityList.FromString("1111"));
-		p.setArea(AreaList.FromString("11111"));
-		p.setSmoking(SmokingList.FromString("111"));
-		p.setDrinking(DrinkingList.FromString("11"));
-		u.getPreferences().add(p);
-		
-		u.resetAlteredValues();
-		
-		String sexstring = "";
-		for (SexualityType t : u.getPreferences().get(0).getSexuality().getList())
+		for (User u : users)
 		{
-			sexstring += t.toString() + ", ";
+			System.out.println("Found user: " + u.getUsername());
 		}
-		System.out.println("You can...");
-		System.out.println("Iterate over the enum lists: " + sexstring);
-		System.out.println("And import/export binary strings: " + u.getPreferences().get(0).getSexuality().toString());
 	}
 }
